1c8e2114a908dca27a503708c73284fee19f5755
Juan Manuel Cardona Granda <juanmanuel.cardonagranda@gmail.com>
2016-03-13 13:10:31 -0500

    Added files via upload
    Code for the Cachematrix assignment of the Third Week of 'R programming course' of Coursera 'Data Science'

---
+++ cachematrix.R
@@ -0,0 +1,33 @@
+## This function caches a matrix than can cache its own inverse
+
+## Essentialy a modified version of "Makevector"
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+}
+
+
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
+}
